6dbc413f5a903a31cca41461e603fb85423c9133
victormuteithia <muteithiavictor@gmail.com>
2023-10-12 01:28:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let customers;
+  if(ride <= 400){
+    customers = 'This one is on me!'
+  }else if(ride > 2000){
+    customers = 'I will gladly take your thirty bucks.'
+  }else if(ride > 2500){
+    customers = 'No can do.'
+  }
+  return customers
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let destination = city === 'NYC'? 'Ok, sounds good.': 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let response;
+  switch(tip){
+    case 'generous':
+      response = 'Thank you so much.';
+    case  'not as generous':
+      response = 'Thank you.';
+    case 'thanks for everything':
+      response = 'Bye';
+  }
+  return response
 }
